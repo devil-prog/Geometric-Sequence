76cbf4f5b702e546bc08c32ed86f3a279c1482ad
devil-prog <krishmikaabisheka1@gmail.com>
2021-07-17 18:43:13 +0530

    edited

--- main.py
+++ main.py
@@ -1,37 +1,44 @@
 import tkinter as tk
+
+#Functions
 class app:
     def __init__(self,e1,e2,e3):
-        self.e1 = e1
-        self.e2 = e2
-        self.e3 = e3
+        self.e1 = int(e1)
+        self.e2 = int(e2)
+        self.e3 = int(e3)
 
-    def get_entry_fields():
-        a = e1
-        b = e2
-        c = e3
-        print(a,b,c)
+    def calculate(self,n):
+        if (self.e2/self.e1) == (self.e3/self.e2):
+            out = self.e1*((self.e2/self.e1)**(int(n)-1))
+            top = tk.Toplevel()
+            top.geometry("250x250")
+            msg = tk.Label(top, text=out)
+            msg.config(font=('Helvetica bold',40))
+            msg.pack()
+            print(out)
+
+def get_entry_fields():
+    a = app(e1.get(),e2.get(),e3.get())
+    a.calculate(en.get())     
+
 top = tk.Tk()
-# Code to add widgets
+#Code to add widgets
+#Added lables
 tk.Label(top, text="Enter first 3 numbers").grid(row=0)
+tk.Label(top, text="Enter Element number").grid(row=1)
 
+#Added entrys
 e1 = tk.Entry(top)
 e2 = tk.Entry(top)
 e3 = tk.Entry(top)
+en = tk.Entry(top)
 
+#Organize entrys
 e1.grid(row=0, column=1)
 e2.grid(row=0, column=2)
 e3.grid(row=0, column=3)
-
-tk.Button(top, text='Submit', command=lambda:[app(e1.get(), e2.get(), e3.get()), app.get_entry_fields()]).grid(row=3, column=1, sticky=tk.W, pady=4)
-#app(e1.get(), e2.get(), e3.get())
-
-
-
-'''def helloCallBack():
-        print( "Hello Python", "Hello World")
-
-        B = tk.Button(top, text ="Hello", command = helloCallBack)
-
-        B.pack()'''
+en.grid(row=1, column=1)
+    
+tk.Button(top, text='Submit', command=get_entry_fields).grid(row=3, column=1, sticky=tk.W, pady=4)
 
 top.mainloop()
