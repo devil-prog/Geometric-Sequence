5d73f1cd5d1f7dd79fda2b545c8135246eb8c1ca
devil-prog <krishmikaabisheka1@gmail.com>
2021-07-23 07:40:15 +0530

    Bug fixed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import *
+import sys
 
 #Functions
 class app:
@@ -10,23 +11,34 @@ class app:
 
     def calculate(self,n):
         if (self.e2/self.e1) == (self.e3/self.e2):
-            out = self.e1*((self.e2/self.e1)**(int(n)-1))
+            try:
+                out = self.e1*((self.e2/self.e1)**(int(n)-1))
+            except:
+                print("Oops!", sys.exc_info()[0], "occurred.")
+                print("Next entry.")
+                print()
             if 10000000000000000 > out:
                 top = tk.Toplevel()
                 top.geometry("720x72")
                 msg = tk.Label(top, text=out)
                 msg.config(font=('Helvetica bold', 40))
                 msg.pack()
+                print(out)
             else:
                 print(out)
 
     def calculate2(self,Tn):
         if (self.e2/self.e1) == (self.e3/self.e2):
-            st1 = int(Tn)/self.e1
+            try:
+                st1 = int(Tn)/self.e1
+            except:
+                print("Oops!", sys.exc_info()[0], "occurred.")
+                print("Next entry.")
+                print()
             r = (self.e2/self.e1)
             i = 1
             j = r
-            while 10000000 > r:
+            while 10000000000000000 > r:
                 r = r*j
                 i = i+1
                 if r == st1:
@@ -41,18 +53,21 @@ class app:
                 if r > st1:
                     print("This number is not in the sequence")
                     break
-            
                 
             
 def get_entry_fields():
     a = app(e1.get(),e2.get(),e3.get())
-    try:
-        a.calculate(en.get())
-    except:
+    if not en.get():
         a.calculate2(eTn.get())
+    else:
+        a.calculate(en.get())
+            
 
 top = tk.Tk()
 top.title("Geometric-Sequence")
+p1 = PhotoImage(file = 'icon.png')
+top.iconphoto(False, p1)
+
 #Code to add widgets
 #Added lables
 tk.Label(top, text="Enter first 3 numbers").grid(row=0)
@@ -60,8 +75,6 @@ tk.Label(top, text="To Find Value of Element",).grid(row=1,column=2)
 tk.Label(top, text="Enter Element number").grid(row=2)
 tk.Label(top, text="To Find Element of Given Value").grid(row=3,column=2)
 tk.Label(top, text="Enter Element value").grid(row=4)
-
-lf = ttk.Labelframe(top, text='Label').grid(row=1)
 
 #Added entrys
 e1 = tk.Entry(top)
