bde5a973ac96e61e8d72cf00c14f0caf02386a84
devil-prog <krishmikaabisheka1@gmail.com>
2021-07-24 08:05:21 +0530

    Upgraded

--- main.py
+++ main.py
@@ -13,56 +13,79 @@ class app:
         if (self.e2/self.e1) == (self.e3/self.e2):
             try:
                 out = self.e1*((self.e2/self.e1)**(int(n)-1))
+                if 10000000000000000 > out:
+                    top = tk.Toplevel()
+                    top.geometry("720x72")
+                    msg = tk.Label(top, text=out)
+                    msg.config(font=('Helvetica bold', 40))
+                    msg.pack()
+                    print(out)
+                else:
+                    print(out)
             except:
                 print("Oops!", sys.exc_info()[0], "occurred.")
                 print("Next entry.")
                 print()
-            if 10000000000000000 > out:
-                top = tk.Toplevel()
-                top.geometry("720x72")
-                msg = tk.Label(top, text=out)
-                msg.config(font=('Helvetica bold', 40))
-                msg.pack()
-                print(out)
-            else:
-                print(out)
 
     def calculate2(self,Tn):
         if (self.e2/self.e1) == (self.e3/self.e2):
             try:
                 st1 = int(Tn)/self.e1
+                r = (self.e2/self.e1)
+                i = 1
+                j = r
+                while 10000000000000000 > r:
+                    r = r*j
+                    i = i+1
+                    if r == st1:
+                        out = i+1
+                        top = tk.Toplevel()
+                        top.geometry("720x72")
+                        msg = tk.Label(top, text=out)
+                        msg.config(font=('Helvetica bold',40))
+                        msg.pack()
+                        print(out)
+                        break
+                    if r > st1:
+                        print("This number is not in the sequence")
+                        break
             except:
                 print("Oops!", sys.exc_info()[0], "occurred.")
                 print("Next entry.")
                 print()
-            r = (self.e2/self.e1)
-            i = 1
-            j = r
-            while 10000000000000000 > r:
-                r = r*j
-                i = i+1
-                if r == st1:
-                    out = i+1
-                    top = tk.Toplevel()
-                    top.geometry("720x72")
-                    msg = tk.Label(top, text=out)
-                    msg.config(font=('Helvetica bold',40))
-                    msg.pack()
-                    print(out)
-                    break
-                if r > st1:
-                    print("This number is not in the sequence")
-                    break
-                
-            
-def get_entry_fields():
-    a = app(e1.get(),e2.get(),e3.get())
+
+def clearNtrys():
+   en.delete(0, END)
+   eTn.delete(0, END) 
+
+def secondopt():
+    a = app(int(ea.get()),int(ea.get())*int(er.get()),int(ea.get())*int(er.get())*int(er.get()))
     if not en.get():
         a.calculate2(eTn.get())
+        clearNtrys()
     else:
         a.calculate(en.get())
-            
+        clearNtrys()
 
+def get_entry_fields():
+    if not e1.get():
+        secondopt()
+    elif not e2.get():
+        secondopt()
+    elif not e3.get():
+        secondopt()
+    else:
+        a = app(e1.get(),e2.get(),e3.get())
+        if not en.get():
+            a.calculate2(eTn.get())
+            clearNtrys()
+        else:
+            a.calculate(en.get())
+            clearNtrys()
+
+def enter(event):
+    get_entry_fields()
+    
 top = tk.Tk()
 top.title("Geometric-Sequence")
 p1 = PhotoImage(file = 'icon.png')
@@ -71,15 +94,20 @@ top.iconphoto(False, p1)
 #Code to add widgets
 #Added lables
 tk.Label(top, text="Enter first 3 numbers").grid(row=0)
-tk.Label(top, text="To Find Value of Element",).grid(row=1,column=2)
-tk.Label(top, text="Enter Element number").grid(row=2)
-tk.Label(top, text="To Find Element of Given Value").grid(row=3,column=2)
-tk.Label(top, text="Enter Element value").grid(row=4)
+tk.Label(top, text="or").grid(row=1, column=2)
+tk.Label(top, text="a :").grid(row=2, column=1, sticky=tk.W)
+tk.Label(top, text="r :").grid(row=2, column=3, sticky=tk.W)
+tk.Label(top, text="To Find Value of Element",).grid(row=3,column=2)
+tk.Label(top, text="Enter Element number").grid(row=4)
+tk.Label(top, text="To Find Element of Given Value").grid(row=5,column=2)
+tk.Label(top, text="Enter Element value").grid(row=6)
 
 #Added entrys
 e1 = tk.Entry(top)
 e2 = tk.Entry(top)
 e3 = tk.Entry(top)
+ea = tk.Entry(top)
+er = tk.Entry(top)
 en = tk.Entry(top)
 eTn = tk.Entry(top)
 
@@ -87,10 +115,13 @@ eTn = tk.Entry(top)
 e1.grid(row=0, column=1)
 e2.grid(row=0, column=2)
 e3.grid(row=0, column=3)
-en.grid(row=2, column=1)
-eTn.grid(row=4, column=1)
+ea.grid(row=2, column=1, sticky=tk.E, padx=20)
+er.grid(row=2, column=3, sticky=tk.E, padx=20)
+en.grid(row=4, column=1)
+eTn.grid(row=6, column=1)
 
 row, column = top.grid_size()    
-tk.Button(top, text='Submit', command=get_entry_fields).grid(row=row+1, column=column, sticky=tk.NS, padx=20, pady=20)
+tk.Button(top, text='Submit', command=get_entry_fields).grid(row=7, column=4, sticky=tk.NS, padx=20, pady=20)
+top.bind('<Return>', enter)
 
 top.mainloop()
